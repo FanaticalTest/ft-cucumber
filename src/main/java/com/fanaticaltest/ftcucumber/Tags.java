61e3d73cd6339c307d609fc352e4b5aa28265b70
Ziridis <samy@kacem.net>
2017-11-30 16:13:58 +0100

    Migrate tags from Factory
    - Migrate
    - Implement test

--- src/main/java/com/fanaticaltest/ftcucumber/Tags.java
+++ src/main/java/com/fanaticaltest/ftcucumber/Tags.java
@@ -1,7 +1,29 @@
 package com.fanaticaltest.ftcucumber;
 
-/**
- * Created by samykacem on 30.11.17.
- */
+import java.util.Collection;
+
 public class Tags {
+
+    public static String getId(Collection<String> tags) {
+        return getSpecificTag(tags, "@id=");
+    }
+
+    public static String getFeature(Collection<String> tags) {
+        return getSpecificTag(tags, "@feature=");
+    }
+
+    public static String getProject(Collection<String> tags) { return getSpecificTag(tags, "@project="); }
+
+    public static String getProjectId(Collection<String> tags) { return getSpecificTag(tags, "@projectid="); }
+
+    public static String getTestSuite(Collection<String> tags) { return getSpecificTag(tags, "@testsuite="); }
+
+    public static String getSpecificTag(Collection<String> tags, String tagPrefix) {
+        for (String tag : tags) {
+            if (tag.toLowerCase().startsWith(tagPrefix)) {
+                return tag.substring(tagPrefix.length(), tag.length());
+            }
+        }
+        return "Unknown or tag not well formatted";
+    }
 }
